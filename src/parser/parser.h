b628595e5620c4f834271e4e7a2746ebdd9f159f
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2023-04-17 18:56:20 -0300

    formatting

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,16 +1,18 @@
 #include "../ast/ast.h"
 
 typedef struct {
-    Lexer *l;
-    Token cur_token;
-    Token peek_token;
-    LinkedList *errors;
+  Lexer *l;
+  Token cur_token;
+  Token peek_token;
+  LinkedList *errors; // TODO: implement as string buffer
+  Expression *(*prefixParseFn[TOKEN_COUNTS])(void);
+  Expression *(*infixParseFns[TOKEN_COUNTS])(Expression *);
 } Parser;
 
-Parser *new_parser(Lexer*);
+Parser *new_parser(Lexer *);
 
-void parser_next_token(Parser*);
+void parser_next_token(Parser *);
 
-void free_parser(Parser*);
+void free_parser(Parser *);
 
-Program *parse_program(Parser*);
+Program *parse_program(Parser *);
