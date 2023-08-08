83deb69953cf5e880df0c70c9d97c4e0b00e9dc9
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2023-08-08 20:02:03 -0300

    Refactored usage options

--- src/main.c
+++ src/main.c
@@ -3,6 +3,14 @@
 #include <stdio.h>
 #include <string.h>
 
+void usage() {
+  printf("Usage: monkey [options] [file]\n");
+  printf("Options:\n");
+  printf("  -i\t\t\tStarts the REPL in interpret mode\n");
+  printf("  -c\t\t\tStarts the REPL in compile mode\n");
+  printf("  -h\t\t\tPrints this help message\n");
+}
+
 ReplMode get_repl_mode(char *flag) {
   if (strncmp(flag, "-i", 2) == 0) {
     return REPL_INTERPRET;
@@ -18,13 +26,25 @@ ReplMode get_repl_mode(char *flag) {
 int main(int argc, char **argv) {
   if (argc == 1) {
     start_repl(REPL_INTERPRET);
-  } else if (argv[1][0] == '-') {
-    ReplMode mode = get_repl_mode(argv[1]);
-    start_repl(mode);
-  } else {
-    //TODO: add support for compiling files
-    eval_file(argv[1]);
+    return 0;
+  } 
+
+  if (argc == 2 && strncmp(argv[1], "-h", 2) == 0) {
+    usage();
+    return 0;
   }
 
+  if (argc == 2 && strncmp(argv[1], "-", 1) == 0) {
+    ReplMode mode = get_repl_mode(argv[1]);
+    start_repl(mode);
+    return 0;
+  }
+
+  if (argc == 3 && strncmp(argv[1], "-", 1) == 0) {
+    eval_file(argv[2]); // TODO: compile file if -c flag is passed
+    return 0;
+  }
+
+  usage();
   return 0;
 }
